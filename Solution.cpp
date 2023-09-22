7edc4408c409bf6477c58214db1dc49b71c5f6a4
Lakshay <lakshaysharmadav1@gmail.com>
2023-09-23 00:17:22 +0530

    next

---
+++ Solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t, i, j;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Mahmoud"<<endl;
+    }
+    else
+    {
+        cout<<"Ehab"<<endl;
+    }
+
+
+
+}
+
